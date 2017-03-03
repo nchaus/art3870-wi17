ca5a61d4529ffc9c3aa96b12660854ce7f5b831d
nchaus <nchaus@horizon.csueastbay.edu>
2017-03-02 17:55:12 -0800

    updated pro2 info
    included the image moving upward

--- Project 2/js/pro2.js
+++ Project 2/js/pro2.js
@@ -17,14 +17,10 @@ $(window).scroll(function(){
 
    console.log("oh glorious Queen, you are " + fromLeft + " times more astronomical!");
 
-   if(fromLeft >= 4500 ){
-     console.log("oh Queen, you have scrolled so far, you must be tired!");
-
-   }else {
-
+   if(fromLeft < 4500 ){
      console.log("going backwards, are we?");
 //passing cars including my car
-     $("#one").css({"left" : 50 + fromLeft *1.1 + "px"});
+     $("#one").css({"left" : 50 + fromLeft *1.1 + "px"});//at 4500 the car is at 4950
      $("#two").css({"left" : 75 + fromLeft * .9 + "px"});
      $("#three").css({"left" : 100 + fromLeft * .99 + "px"});
 //passing trees
@@ -32,12 +28,72 @@ $(window).scroll(function(){
      $("#t2").css({"left" : 400 + fromLeft * .8 + "px"});
      $("#t3").css({"left" : 600 + fromLeft * .8 + "px"});
      $("#t4").css({"left" : 800 + fromLeft * .8 + "px"});
+
+// person reset
+$("#person").css({"display":"none" });
+
    }
+
+   if(fromLeft > 4501 && fromLeft < 5000){
+     console.log("oh Queen, you have scrolled so far, you must be tired!");
+
+     $("#person").css({
+                      "left" : 75 + fromLeft *1.1 + "px",
+                      "top":600 - fromLeft * 0.01 + "px",
+                      "display":"block"
+                      });
+      $("#cloud").css({
+                      "top":100 - fromLeft * 0.01 + "px",
+                      "left": 800 + fromLeft *1.1 + "px",
+                             "display":"block"
+                     });
+   }
+
+   if(fromLeft > 5001 && fromLeft < 7500){
+     console.log("oh Queen, you have scrolled so far, you must be tired!");
+
+     $("#person").css({
+                      "left" : 75 + fromLeft *1.1 + "px",
+                      "top":600 - fromLeft * 0.01 + "px",
+                      "display":"block"
+                      });
+      $("#cloud").css({
+                      "top":100 - fromLeft * 0.01 + "px",
+                      "left": 800 + fromLeft *1.1 + "px",
+                             "display":"block"
+                     });
+   }
+
+
+
+
+
+
+   if(fromLeft > 7500 && fromLeft < 8000 ){
+     console.log("oh Queen, you have scrolled so far, you must be tired!");
+     $("#person").css({
+                      "left" : 50 + fromLeft *1.1 + "px",
+                      "top":8000 - fromLeft  + "px",
+                      "display":"block"
+                      });
+   }
+
+   if(fromLeft > 8000 ){
+     console.log("oh Queen, you have scrolled so far, you must be tired!");
+     $("#person").css({
+                      "left" : 50 + fromLeft *1.1 + "px",
+                      "top":"0",
+                      "display":"block"
+                      });
+   }
+
+
 
    if(fromLeft > 4600 ){
 //person flying
-     $("#person").css({"left" : 4600 + fromLeft * .6 + "px"});
+    // $("#person").css({"left" : 4600 + fromLeft * .6 + "px"});
 
+   }
 
 
 
